9ac18d49cd830d4afeea734db151bdf06d55cd6f
junterbu <jan.unterbuchschachner@tuwien.ac.at>
2025-01-29 10:28:21 +0100

    Backup: 29.01.2025 10:28:21,64

--- Excel.js
+++ Excel.js
@@ -5,7 +5,7 @@ export function generatePDFReport(mischgutName, eimerWerte, bitumengehalt, Rohdi
 
     // Titel
     pdf.setFontSize(16);
-    pdf.text("Laborbericht - Asphaltmischung", 105, startY, { align: "center" });
+    pdf.text("Virtueller Laborbericht - Asphaltmischung", 105, startY, { align: "center" });
     startY += 10;
 
     // Mischgut
@@ -14,7 +14,7 @@ export function generatePDFReport(mischgutName, eimerWerte, bitumengehalt, Rohdi
     startY += 10;
 
     // Eimerwerte Tabelle
-    pdf.text("Eimerwerte:", 10, startY);
+    pdf.text("Eimerwerte [%]:", 10, startY);
     startY += 5;
 
     const eimerHeaders = ["Füller", "0/2", "2/4", "4/8", "8/11", "11/16", "16/22", "22/32"];
@@ -26,11 +26,21 @@ export function generatePDFReport(mischgutName, eimerWerte, bitumengehalt, Rohdi
     });
     startY = pdf.lastAutoTable.finalY + 10;
 
+    // Sieblinie
+    if (sieblinieCanvas) {
+        pdf.text("Sieblinie:", 10, startY);
+        startY += 5;
+        const sieblinieImage = sieblinieCanvas.toDataURL("image/png");
+        pdf.addImage(sieblinieImage, "PNG", 10, startY, 180, 100);
+    }
+
+    startY += 100;
+
     // Bindemittel und Rohdichten
-    pdf.text("Bindemittelgehalt:", 10, startY);
+    pdf.text("Bindemittelgehalt [%]:", 10, startY);
     startY += 5;
 
-    const biHeaders = ["Bitumengehalt 1 (%)", "Bitumengehalt 2 (%)", "Bitumengehalt 3 (%)"];
+    const biHeaders = ["Bitumengehalt 1", "Bitumengehalt 2", "Bitumengehalt 3"];
     const biData = [bitumengehalt.flat()];
     pdf.autoTable({
         startY,
@@ -40,7 +50,7 @@ export function generatePDFReport(mischgutName, eimerWerte, bitumengehalt, Rohdi
     startY = pdf.lastAutoTable.finalY + 10;
 
     // Bindemittel und Rohdichten
-    pdf.text("Rohdichten:", 10, startY);
+    pdf.text("Rohdichten [g/cm³]:", 10, startY);
     startY += 5;
 
     const rohHeaders = ["Rohdichte 1", "Rohdichte 2", "Rohdichte 3"];
@@ -53,7 +63,7 @@ export function generatePDFReport(mischgutName, eimerWerte, bitumengehalt, Rohdi
     startY = pdf.lastAutoTable.finalY + 10;    
 
     // Raumdichten
-    pdf.text("Raumdichten:", 10, startY);
+    pdf.text("Raumdichten [g/cm³]:", 10, startY);
     startY += 5;
 
     const raumHeaders = ["R1-1", "R1-2", "R1-3", "R1-4", "R2-1", "R2-2", "R2-3", "R2-4", "R3-1", "R3-2", "R3-3", "R3-4"];
@@ -64,14 +74,6 @@ export function generatePDFReport(mischgutName, eimerWerte, bitumengehalt, Rohdi
         body: raumData,
     });
     startY = pdf.lastAutoTable.finalY + 10;
-
-    // Sieblinie
-    if (sieblinieCanvas) {
-        pdf.text("Sieblinie:", 10, startY);
-        startY += 5;
-        const sieblinieImage = sieblinieCanvas.toDataURL("image/png");
-        pdf.addImage(sieblinieImage, "PNG", 10, startY, 180, 80);
-    }
 
     // Speichern der PDF
     pdf.save("Laborbericht.pdf");
